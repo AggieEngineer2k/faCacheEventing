aff565e5097a83826f98402d91e766542dc21557
Justin L. Brown <justin.brown@cpchem.com>
2022-03-21 15:00:23 -0500

    Added from/to datetimes.

--- docs/demo-rig.py
+++ docs/demo-rig.py
@@ -9,15 +9,6 @@ from textwrap import fill
 from tkinter import *
 import tksheet
 
-# Connect to Azure Cache for Redis server.
-redisClient = redis.Redis(host='rcCacheEventing.redis.cache.windows.net', port=6380, db=0, ssl=True, password='cQM2lM2YO5zHSAkdUrJTNzqdzUuMF5TRCAzCaF16CqU=', decode_responses=True)
-
-# Connect to Event Grid Topic.
-key = 'sMtf3ieNHThwCkXJMTcl3Rb/Yo9OaUZalEj4XBLbANY='
-endpoint = 'https://egtcacheeventing.centralus-1.eventgrid.azure.net/api/events'
-credential = AzureKeyCredential(key)
-eventGridClient = EventGridPublisherClient(endpoint, credential)
-
 window = Tk()
 window.title("Cache Eventing Demo Rig")
 
@@ -52,17 +43,29 @@ labelResinName.grid(row=3, column=0, sticky='e')
 entryResinName = Entry(entryFrame)
 entryResinName.insert(0, "HHM 5502BN")
 entryResinName.grid(row=3, column=1)
+labelFromDateTime = Label(entryFrame, text="From:")
+labelFromDateTime.grid(row=4, column=0, sticky='e')
+entryFromDateTime = Entry(entryFrame)
+entryFromDateTime.insert(0, (datetime.utcnow() - timedelta(hours=2)).isoformat(' '))
+entryFromDateTime.grid(row=4, column=1)
+labelToDateTime = Label(entryFrame, text="To:")
+labelToDateTime.grid(row=5, column=0, sticky='e')
+entryToDateTime = Entry(entryFrame)
+entryToDateTime.insert(0, datetime.utcnow().isoformat(' '))
+entryToDateTime.grid(row=5, column=1)
 
+buttonGetKeys = Button(buttonFrame, text="Get Sorted Set Keys")
+buttonGetKeys.pack(fill='x')
 buttonFetchAll = Button(buttonFrame, text="Fetch All")
 buttonFetchAll.pack(fill='x')
+buttonFetchCustomDateTimeRange = Button(buttonFrame, text="Fetch From/To")
+buttonFetchCustomDateTimeRange.pack(fill='x')
 buttonFetchLastTwoHours = Button(buttonFrame, text="Fetch Last Two Hours")
 buttonFetchLastTwoHours.pack(fill='x')
 buttonFetchLatest = Button(buttonFrame, text="Fetch Latest")
 buttonFetchLatest.pack(fill='x')
 buttonPublishEvent = Button(buttonFrame, text="Publish Event to Event Grid")
 buttonPublishEvent.pack(fill='x')
-buttonGetKeys = Button(buttonFrame, text="Get Sorted Set Keys")
-buttonGetKeys.pack(fill='x')
 
 img = PhotoImage(file='docs/Architecture.png')
 Label(
@@ -73,9 +76,26 @@ Label(
 text_box = Text(textFrame, height=10)
 text_box.pack(fill='x')
 
+try:
+    # Connect to Azure Cache for Redis server.
+    redisClient = redis.Redis(host='rcCacheEventing.redis.cache.windows.net', port=6380, db=0, ssl=True, password='cQM2lM2YO5zHSAkdUrJTNzqdzUuMF5TRCAzCaF16CqU=', decode_responses=True)
+    text_box.insert(END, "Connected to Azure Cache for Redis.\n")
+except:
+    text_box.insert(END, "[ERROR] Could not connect to Azure Cache for Redis!\n")
+
+try:
+    # Connect to Event Grid Topic.
+    key = 'sMtf3ieNHThwCkXJMTcl3Rb/Yo9OaUZalEj4XBLbANY='
+    endpoint = 'https://egtcacheeventing.centralus-1.eventgrid.azure.net/api/events'
+    credential = AzureKeyCredential(key)
+    eventGridClient = EventGridPublisherClient(endpoint, credential)
+    text_box.insert(END, "Connected to Azure Event Grid.\n")
+except:
+    text_box.insert(END, "[ERROR] Could not connect to Azure Event Grid!\n")
+
 def appendRedisStatistics():
     info = redisClient.info()
-    text_box.insert(END, "--- Redis Info ---\n")
+    text_box.insert(END, "\n--- Redis Info ---\n")
     for metric in [
         'redis_version',
         'used_memory_human',
@@ -83,7 +103,7 @@ def appendRedisStatistics():
         'maxmemory_human']:
         text_box.insert(END, "{metric}: {value}\n".format(metric = metric, value = info[metric]))
 
-sheet = tksheet.Sheet(sheetFrame, width=800, height=400)
+sheet = tksheet.Sheet(sheetFrame, width=800, height=200)
 sheet.pack()
 sheet.enable_bindings((
     "single_select",
@@ -109,14 +129,15 @@ def buttonFetchCallback(fromscore, toscore, reverse : bool = False, top : int = 
         records = redisClient.zrangebyscore(key,fromscore,toscore,withscores=True)
     elif reverse is False and top is not None:
         text_box.insert(END, "ZRANGEBYSCORE {key} {min} {max} WITHSCORES LIMIT 0 {count}\n".format(key = key, min = fromscore, max = toscore, count = top))
-        records = redisClient.zrangebyscore(key,fromscore,toscore,start=0,num=top,withscores=True)
+        records = redisClient.zrangebyscore(key,fromscore,toscore,withscores=True,start=0,num=top)
     elif reverse is True and top is None:
         text_box.insert(END, "ZREVRANGEBYSCORE {key} +inf -inf WITHSCORES\n".format(key = key))
         records = redisClient.zrevrangebyscore(key,'+inf','-inf',withscores=True)
     elif reverse is True and top is not None:
         text_box.insert(END, "ZREVRANGEBYSCORE {key} +inf -inf WITHSCORES LIMIT 0 {count}\n".format(key = key, count = top))
-        records = redisClient.zrevrangebyscore(key,'+inf','-inf',start=0,num=top,withscores=True)
+        records = redisClient.zrevrangebyscore(key,'+inf','-inf',withscores=True,start=0,num=top)
 
+    text_box.insert(END, "ZCARD {key}\n".format(key = key))
     sheet_data = []
     sheet_data.append([
         "score",
@@ -147,11 +168,11 @@ def buttonFetchCallback(fromscore, toscore, reverse : bool = False, top : int = 
     sheet.set_all_cell_sizes_to_text()
 
     cardinality = redisClient.zcard('site:{site}:extruder:{extruder}'.format(site = entrySite.get(), extruder = entryExtruder.get()))
-    text_box.insert(END, "Results: {len_records} (of {cardinality})\n".format(len_records = len(records), cardinality = cardinality))
+    text_box.insert(END, "\nResults: {len_records} (of {cardinality})\n".format(len_records = len(records), cardinality = cardinality))
     appendRedisStatistics()
 
 def buttonPublishEventCallback():
-    event = EventGridEvent(subject="PoC",event_type="Azure.Sdk.Demo",data_version="2.0",data={
+    event = EventGridEvent(subject="PoC",event_type="PoC.Events.NewPrediction",data_version="1.0",data={
         "timestamp": datetime.utcnow().isoformat(' '),
         "site": entrySite.get(), 
         "extruder": entryExtruder.get(), 
@@ -169,11 +190,16 @@ def buttonGetKeysCallback():
     sheet_data = []
     sheet_data.append([
         "key",
+        "memory",
         "cardinality"])
     for key in redisClient.scan_iter('site:*:extruder:*'):
+        text_box.insert(END, "MEMORY USAGE {key}\n".format(key = key))
+        text_box.insert(END, "ZCARD {key}\n".format(key = key))
+        memory = redisClient.memory_usage(key)
         cardinality = redisClient.zcard(key)
         sheet_data.append([
             key,
+            memory,
             cardinality])
 
     sheet.set_sheet_data(sheet_data)
@@ -185,5 +211,8 @@ buttonFetchLatest.configure(command=lambda: buttonFetchCallback('-inf', '+inf', 
 buttonFetchLastTwoHours.configure(command=lambda: buttonFetchCallback((datetime.utcnow() - timedelta(hours=2) - datetime(1,1,1)).total_seconds() * 10**7,'+inf'))
 buttonPublishEvent.configure(command=buttonPublishEventCallback)
 buttonGetKeys.configure(command=buttonGetKeysCallback)
+buttonFetchCustomDateTimeRange.configure(command=lambda: buttonFetchCallback(
+    (datetime.strptime(entryFromDateTime.get(), '%Y-%m-%d %H:%M:%S.%f') - datetime(1,1,1)).total_seconds() * 10**7,
+    (datetime.strptime(entryToDateTime.get(), '%Y-%m-%d %H:%M:%S.%f') - datetime(1,1,1)).total_seconds() * 10**7))
 
 window.mainloop()
